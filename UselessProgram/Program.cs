4c829e5f93428d5dc89e50a32e3046ba5531e83c
Adnan Turan <adnanturan2000@gmail.com>
2024-10-24 20:10:17 +0100

    Fixed File Structure

--- UselessProgram/Program.cs
+++ UselessProgram/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 
-namespace Program {
 
-    static class Program {
-        
-        static void Main(string[] args) {
-
+namespace UsefulProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
             Console.WriteLine("Hello World!");
-
+            Console.WriteLine($"5 + 3 = {MathsOperations.MathFunctions.Add(5, 3)}");
+            Console.WriteLine($"5 - 3 = {MathsOperations.MathFunctions.Subtract(5, 3)}");
+            Console.WriteLine($"5 * 3 = {MathsOperations.MathFunctions.Multiply(5, 3)}");
+            Console.WriteLine($"5 / 3 = {MathsOperations.MathFunctions.Divide(5, 3)}");
         }
-
     }
 
 }
--- UselessProgram/obj/Debug/net8.0/UselessProgram.AssemblyInfo.cs
+++ UselessProgram/obj/Debug/net8.0/UselessProgram.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UselessProgram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+023bf931e44b0c9762e18d492a089828efa379cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aca8b4722fb63bd91f61a4793554f30a5b11b088")]
 [assembly: System.Reflection.AssemblyProductAttribute("UselessProgram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UselessProgram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
