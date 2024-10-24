aca8b4722fb63bd91f61a4793554f30a5b11b088
Adnan Turan <adnanturan2000@gmail.com>
2024-10-24 12:22:30 +0100

    First Commit

---
+++ UselessProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Program {
+
+    static class Program {
+        
+        static void Main(string[] args) {
+
+            Console.WriteLine("Hello World!");
+
+        }
+
+    }
+
+}
